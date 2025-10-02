a34dcf79dc868834776c60f33cf3ecc02bd15488
Abhishek Kumar <abhishek23cu@gmail.com>
2025-10-02 21:08:09 +0530

    Time: 634 ms (49.31%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -0,0 +1,9 @@
+SELECT 
+    ROUND(100 * SUM(CASE WHEN order_date = customer_pref_delivery_date THEN 1 ELSE 0 END) / COUNT(*), 2) 
+    AS immediate_percentage
+FROM (
+    SELECT *,
+           RANK() OVER (PARTITION BY customer_id ORDER BY order_date) AS rnk
+    FROM Delivery
+) t
+WHERE rnk = 1;
